a532a20cde790987da4987a0dde488a9d60461bc
Aron Hrafnsson <aronhr98@gmail.com>
2019-05-06 13:50:43 +0000

    house maping and templating

---
+++ house/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="house-index"),
+    path('<int:id>', views.get_house_by_id, name="house-details"),
+]
--- house/views.py
+++ house/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'house/index.html', {
+        'houses': 'this is all houses'
+    })
+
+
+def get_house_by_id(request, id):
+    return render(request, 'house/house.html', {
+        'house': 'this is house with id: ' + str(id)
+    })
