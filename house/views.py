f5e2e90d1b9159969f11504bf147d65407d13cad
adambjorgvins <adambjorgvinsson@gmail.comlscd ..hellocd max_nums.py git config --global user.name adambjorgvinsgit config --global user.email adambjorgvinsson@gmail.com>
2019-05-07 20:35:24 +0000

    leitas_siun_fyrir_upphafssidu

--- house/views.py
+++ house/views.py
@@ -4,7 +4,26 @@ from profiles.models import User
 
 
 def index(request):
-    context = {'houses': House.objects.all()}
+    p = PostalCodes.objects.all()
+    context = {
+        'houses': House.objects.all(),
+        'types': HouseType.objects.all(),
+        'towns': PostalCodes.objects.all(),
+        'filter': {
+            'postal_codes': request.POST.getlist('postal_codes'),
+            'lyfta': request.POST.get('lyfta'),
+            'rooms': request.POST.get('rooms')
+
+        },
+    }
+    print(str(p[0].id) in request.POST.getlist('postal_codes'))
+    print("req", request)
+    print("post", request.POST.get('types'))
+    print("post", request.POST.getlist('postal_codes'))
+
+    print("get", request.GET)
+    print("body", request.body)
+
     return render(request, 'house/index.html', context)
 
 
@@ -12,3 +31,4 @@ def get_house_by_id(request, id):
     return render(request, 'house/house.html', {
         'house': get_object_or_404(House, pk=id),
     })
+
