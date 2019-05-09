6e92a526bb725ffde52caa949bb49f6a3b059e86
Aron Hrafnsson <aronhr98@gmail.com>
2019-05-09 18:57:48 +0000

    Bætti við fields í profile

--- login/forms/reg_form.py
+++ login/forms/reg_form.py
@@ -7,7 +7,7 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(label='Skírnafn', help_text='Settu inn skírnafn')
     last_name = forms.CharField(label='Eftirnafn', help_text='Settu inn eftirnafn')
     email = forms.EmailField(label='Netfang', help_text='Settu inn netfang')
-
+    
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
