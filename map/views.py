06ddc764e1b83a275853958f72f0e3f2fbcf3ab0
baldvinbui <baldvin.bui@gmail.com>
2019-05-09 20:40:13 +0000

    map bros

--- map/views.py
+++ map/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from house.models import House
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="my-application")
+location = geolocator.geocode("Daltún 17")
+#print(location.address)
+#print((location.latitude, location.longitude))
 
 
 # Create your views here.
 def index(request):
+    geo = []
+    for x in House.objects.all():
+        try:
+            print(x.address + " " + x.street_nr)
+            location = geolocator.geocode(x.address + " " + x.street_nr)
+            print((location.latitude, location.longitude))
+            geo.append(f"{{lat: {location.latitude}, lng: {location.longitude}}},")
+        except:
+            pass
+    geo[-1] = geo[-1][:-1]
     return render(request, 'map/index.html', {
-        'map': 'this is a cool map u know'
+        'map': 'this is a cool map u know',
+        'ad': geo
     })
