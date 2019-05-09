e296a27f9ce52f9314cf37eadc8a9e5dc87460b3
Aron Hrafnsson <aronhr98@gmail.com>
2019-05-09 18:34:29 +0000

    cleaned typos and unused code

--- login/forms/reg_form.py
+++ login/forms/reg_form.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import ModelForm
-from profiles.models import Profile
 
 
 class SignUpForm(UserCreationForm):
@@ -13,9 +11,3 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-
-
-class ProfileInit(ModelForm):
-    class Meta:
-        model = Profile
-        exclude = ['id', 'kt', 'dob', 'user', 'profile_pic', 'phone', 'address']
