56eb79d48e0dbd98164577a94d6866f57657232c
baldvinbui <baldvin.bui@gmail.com>
2019-05-13 23:18:19 +0000

    map_bug_fixes

--- map/views.py
+++ map/views.py
@@ -9,7 +9,7 @@ def index(request):
     for x in House.objects.all():
         try:
             location = geolocator.geocode(x.address + " " + x.street_nr)
-            geo["houses"].append({'id': x.id, 'geo': {'lat': location.latitude, 'lng': -21.7800444001661}, 'address': x.address + ' ' + x.street_nr, 'image': x.houseimage_set.first})
+            geo["houses"].append({'id': x.id, 'geo': {'lat': location.latitude, 'lng': location.longitude}, 'address': x.address + ' ' + x.street_nr, 'image': x.houseimage_set.first})
         except:
             continue
     return render(request, 'map/index.html', geo)
