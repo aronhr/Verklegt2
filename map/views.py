846704bcf3bab3658e2ac799a631d8f1e09ffa7b
baldvinbui <baldvin.bui@gmail.com>
2019-05-13 14:56:44 +0000

    mapmap

--- map/views.py
+++ map/views.py
@@ -4,23 +4,19 @@ from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="my-application")
 
-#print(location.address)
-#print((location.latitude, location.longitude))
-
 
 # Create your views here.
 def index(request):
     geo = []
     for x in House.objects.all():
         try:
-            print(x.address + " " + x.street_nr)
+            #print(x.address + " " + x.street_nr)
+            # print((location.latitude, location.longitude))
             location = geolocator.geocode(x.address + " " + x.street_nr)
-            print((location.latitude, location.longitude))
-            #geo.append(f"{{lat: {location.latitude}, lng: {location.longitude}}},")
-            geo.append(["Linkur", location.latitude, location.longitude, x.address + " " + x.street_nr])
+            geo.append(['Linkur', location.latitude, location.longitude, x.address + '' + x.street_nr])
         except:
             pass
-    # geo[-1] = geo[-1][:-1]
+    #geo[-1] = geo[-1][:-1]
     return render(request, 'map/index.html', {
         'map': 'this is a cool map u know',
         'ad': geo
