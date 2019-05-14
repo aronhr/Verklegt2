369dafe8cf8bffcff5df1e21f3d0e6ba1c814b98
baldvinbui <baldvin.bui@gmail.com>
2019-05-14 11:48:25 +0000

    Lagaði staðsetningar
    Leitar bara á íslandi og heimilisföng eru í réttu póstnúmeri núna

--- map/views.py
+++ map/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from house.models import House
 from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="my-application")
+geolocator = Nominatim(user_agent="my-application", format_string="%s, Iceland")
 
 
 def index(request):
     geo = {"houses": []}
     for x in House.objects.all():
         try:
-            location = geolocator.geocode(x.address + " " + x.street_nr)
+            location = geolocator.geocode(x.address + " " + x.street_nr + ", " + str(x.p_code))
             geo["houses"].append({'id': x.id, 'geo': {'lat': location.latitude, 'lng': location.longitude}, 'address': x.address + ' ' + x.street_nr, 'image': x.houseimage_set.first})
         except:
             continue
